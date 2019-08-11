2e21fee07a48e352f6270b786ab35c3b83c8fa7c
Joe Becher <jwbecher@drazisil.com>
2019-08-10 23:29:06 -0400

    install dependencies

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Install dependencies
+sudo apt install -y binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev
+
 # Run Tests
-./tests/test
+./tests/test || echo "Tests failed"
 
 # Run tests though kcov
 ./kcov --include-path=./codecov coverage/ tests/test
