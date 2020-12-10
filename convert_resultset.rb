1e992446dcbecfd73e54fcd4ea9b13da8bca732c
Tom Hu <tomhu1096@gmail.com>
2020-12-10 11:05:11 -0500

    Add a conversion for Codecov

---
+++ convert_resultset.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'JSON'
+
+def convert_resultset(file_path)
+  result = JSON.parse(File.read(file_path))
+
+  result = result['/bin/bash ./codecov']
+  result['coverage'].each do |key, value|
+    result['coverage'][key] = value.each_with_index.map { |i, v| [v, i] }.to_h
+  end
+
+  File.open('./coverage/converted_resultset.json', 'w') { |f| f.write result }
+end
+
+convert_resultset(ARGV[0])
