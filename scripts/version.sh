27ea4a30c54997aa0deca35baa5f942ee6de9aa2
Tom Hu <tomhu1096+gpg@gmail.com>
2021-04-12 23:15:36 -0400

    Remove version dependency on date and SHA

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,14 +2,9 @@
 
 set -x
 
-change_bash_file () {
-  sed -i -e "s|.*VERSION=.*|VERSION=\"$RELEASE_TAG\"|g" codecov
-  rm codecov-e || :
-}
-
-change_env_file () {
-  sed -i -e "s|.*VERSION=.*|VERSION=\"$RELEASE_TAG\"|g" env
-  rm env-e || :
+clear_branch () {
+  # Clear changes
+  git stash
 }
 
 update_branch () {
@@ -26,16 +21,7 @@ tag_and_push () {
   git push origin "$RELEASE_TAG"
 }
 
-clear_branch () {
-  # Clear changes
-  git stash
-}
-
 clear_branch
 update_branch
-short_git=$(git rev-parse --short HEAD)
-date=$(date +%Y%m%d)
-RELEASE_TAG="$date-$short_git"
-change_bash_file
-change_env_file
+RELEASE_TAG=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2)
 tag_and_push
